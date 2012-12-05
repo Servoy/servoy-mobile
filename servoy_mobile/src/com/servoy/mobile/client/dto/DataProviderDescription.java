4b9c5924c85495438f6a2895886bab6dfc462c9f
Jan Blok <jblok@servoy.com>
2012-12-05 09:26:22 +0000

    fix for old issue

--- servoy_mobile/src/com/servoy/mobile/client/dto/DataProviderDescription.java
+++ servoy_mobile/src/com/servoy/mobile/client/dto/DataProviderDescription.java
@@ -1,21 +1,21 @@
 package com.servoy.mobile.client.dto;
 
 /*
-This file belongs to the Servoy development and deployment environment, Copyright (C) 1997-2012 Servoy BV
+ This file belongs to the Servoy development and deployment environment, Copyright (C) 1997-2012 Servoy BV
 
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License as published by the Free
-Software Foundation; either version 3 of the License, or (at your option) any
-later version.
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Affero General Public License as published by the Free
+ Software Foundation; either version 3 of the License, or (at your option) any
+ later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License along
-with this program; if not, see http://www.gnu.org/licenses or write to the Free
-Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ You should have received a copy of the GNU Affero General Public License along
+ with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ */
 
 import com.google.gwt.core.client.JavaScriptObject;
 
@@ -32,7 +32,9 @@ public class DataProviderDescription extends JavaScriptObject
 	public static final int UUID_COLUMN = 4;
 	public static final int EXCLUDED_COLUMN = 8;
 
-	protected DataProviderDescription() {}
+	protected DataProviderDescription()
+	{
+	}
 
 	public final native String getName() /*-{
 		return this.name;
@@ -43,9 +45,11 @@ public class DataProviderDescription extends JavaScriptObject
 	}-*/;
 
 	public final native int getFlags() /*-{
+		if (!this.flags)
+			return 0;
 		return this.flags;
 	}-*/;
-	
+
 	public final native int getType() /*-{
 		return this.type;
 	}-*/;
@@ -58,21 +62,21 @@ public class DataProviderDescription extends JavaScriptObject
 		return this.dplength;
 	}-*/;
 
-	public final boolean isPK() 
+	public final boolean isPK()
 	{
 		return ((getFlags() & PK_COLUMN) == PK_COLUMN);
 	}
 
-	public final boolean isRowIdent() 
+	public final boolean isRowIdent()
 	{
 		return ((getFlags() & USER_ROWID_COLUMN) == USER_ROWID_COLUMN);
 	}
-	
-	public final boolean isUUID() 
+
+	public final boolean isUUID()
 	{
 		return ((getFlags() & UUID_COLUMN) == UUID_COLUMN);
 	}
-	
+
 	public static DataProviderDescription newInstance(String name, int t)
 	{
 		DataProviderDescription dpd = JavaScriptObject.createObject().cast();
